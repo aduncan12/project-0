063ab7bcf429398bd5b1b1dceda4276347c775a7
Alan <a386226@gmail.com>
2018-08-18 17:10:47 -0700

    got the buttons to work exclamation point

--- js/main.js
+++ js/main.js
@@ -1,4 +1,43 @@
+var mainDivs = $('main').children()
+console.log(mainDivs);
+
+var zFlyers = [];
+for(i = 0; i < mainDivs.length; i++) {
+    zFlyers[i] = $(mainDivs[i]).css("z-index");
+    console.log(zFlyers);
+}
+
 $('#about').on('click', function() {
     console.log('hi');
+    $('.about').css("z-index", zFlyers.length++);
+});
 
-})
+$('#projects').on('click', function() {
+    console.log('hi');
+    $('.projects').css("z-index", zFlyers.length++);
+});
+
+$('#testimonials').on('click', function() {
+    console.log('hi');
+    $('.testimonials').css("z-index", zFlyers.length++);
+});
+
+$('#contact').on('click', function() {
+    console.log('hi');
+    $('.contact').css("z-index", zFlyers.length++);
+});
+
+// var testTimer = $(function() {
+//     $('#test1').fadeIn(5000).fadeOut(5000, function () {
+//         $('#test2').fadeIn(5000).fadeOut(5000, function () {
+//              $('#test3').fadeIn(5000).fadeOut(5000)
+//         })
+//     })
+// })
+
+// setInterval(function() {
+//     testTimer
+// })
+
+
+
