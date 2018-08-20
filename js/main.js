b2d10d1046f9ea92067ee14d3059459bf079ad2a
Alan <a386226@gmail.com>
2018-08-19 18:56:19 -0700

    media queries and timer done

--- js/main.js
+++ js/main.js
@@ -5,7 +5,12 @@ for(i = 0; i < mainDivs.length; i++) {
     zFlyers[i] = $(mainDivs[i]).css("z-index");
 }
 
+// button links to flyers //
 $('#about').on('click', function() {
+    $('.about').css("z-index", zFlyers.length++);
+});
+
+$('#aboutNav').on('click', function() {
     $('.about').css("z-index", zFlyers.length++);
 });
 
@@ -13,7 +18,15 @@ $('#projects').on('click', function() {
     $('.projects').css("z-index", zFlyers.length++);
 });
 
+$('#projectsNav').on('click', function() {
+    $('.projects').css("z-index", zFlyers.length++);
+});
+
 $('#testimonials').on('click', function() {
+    $('.testimonials').css("z-index", zFlyers.length++);
+});
+
+$('#testimonialsNav').on('click', function() {
     $('.testimonials').css("z-index", zFlyers.length++);
 });
 
@@ -21,22 +34,28 @@ $('#contact').on('click', function() {
     $('.contact').css("z-index", zFlyers.length++);
 });
 
+$('#contactNav').on('click', function() {
+    $('.contact').css("z-index", zFlyers.length++);
+});
+// button links to flyers //
 
-var testTimer = $(function() {
-    $('#test1').fadeIn(5000).fadeOut(5000, function () {
-        $('#test2').fadeIn(5000).fadeOut(5000, function () {
-            $('#test3').fadeIn(5000).fadeOut(5000, function() {
-                $(testTimer)
-            })             
-        })
-    })
-})
+// testimonial timer //
 
-
-// setInterval(function() {
+// var testTimer =
+// $(document).ready(function() {
 //     $('#test1').fadeIn(5000).fadeOut(5000, function () {
 //         $('#test2').fadeIn(5000).fadeOut(5000, function () {
-//              $('#test3').fadeIn(5000).fadeOut(5000)            
+//             $('#test3').fadeIn(5000).fadeOut(5000, function() {
+//             })
 //         })
 //     })
-// }, 1000);
+// })
+
+
+setInterval(function() {
+    $('#test1').fadeIn(5000).fadeOut(5000, function () {
+        $('#test2').fadeIn(5000).fadeOut(5000, function () {
+             $('#test3').fadeIn(5000).fadeOut(5000)            
+        })
+    })
+}, 30000);
