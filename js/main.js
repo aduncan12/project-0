bd24cb8ffcd511e104bb3a8f1a2277cc70254e5b
Alan <a386226@gmail.com>
2018-08-19 19:22:34 -0700

    cleaned up some code

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ for(i = 0; i < mainDivs.length; i++) {
     zFlyers[i] = $(mainDivs[i]).css("z-index");
 }
 
-// button links to flyers //
+// button links to flyers 
 $('#about').on('click', function() {
     $('.about').css("z-index", zFlyers.length++);
 });
@@ -37,25 +37,14 @@ $('#contact').on('click', function() {
 $('#contactNav').on('click', function() {
     $('.contact').css("z-index", zFlyers.length++);
 });
-// button links to flyers //
+// button links to flyers 
 
-// testimonial timer //
-
-// var testTimer =
-// $(document).ready(function() {
-//     $('#test1').fadeIn(5000).fadeOut(5000, function () {
-//         $('#test2').fadeIn(5000).fadeOut(5000, function () {
-//             $('#test3').fadeIn(5000).fadeOut(5000, function() {
-//             })
-//         })
-//     })
-// })
-
-
+// testimonials 
 setInterval(function() {
-    $('#test1').fadeIn(5000).fadeOut(5000, function () {
-        $('#test2').fadeIn(5000).fadeOut(5000, function () {
-             $('#test3').fadeIn(5000).fadeOut(5000)            
+    $('#test1').fadeIn(3500).fadeOut(3500, function () {
+        $('#test2').fadeIn(3500).fadeOut(3500, function () {
+             $('#test3').fadeIn(3500).fadeOut(3500)            
         })
     })
-}, 30000);
+}, 21000);
+// testimonials
